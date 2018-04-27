f1b22805286595968d77727e87d7ba02af95ba4e
Lukas Lorimer <lukas.lorimer@gmail.com>
2018-04-27 12:01:24 -0400

    Add some functions to stdio

--- libyaclib/stdio.c
+++ libyaclib/stdio.c
@@ -1,14 +1,40 @@
+#include "stdbool.h"
 #include "stdio.h"
 #include "string.h"
 #include "unistd.h"
 
-int getchar() {
+static FILE stdin_obj = {0, false, 0, 0};
+static FILE stdout_obj = {1, false, 0, 0};
+static FILE stderr_obj = {2, false, 0, 0};
+
+FILE *stdin  = &stdin_obj;
+FILE *stdout = &stdout_obj;
+FILE *stderr = &stderr_obj;
+
+int feof(FILE *stream) {
+  return stream->eof;
+}
+
+int ferror(FILE *stream) {
+  return stream->err;
+}
+
+void clearerr(FILE *stream) {
+  stream->eof = false;
+  stream->err = 0;
+}
+
+int fgetc(FILE *stream) {
   unsigned char buf;
-  const ssize_t s = read(0, &buf, 1);
+  const ssize_t s = read(stream->fd, &buf, 1);
   if (s <= 0) {
     return EOF;
   }
   return buf;
+}
+
+int getchar() {
+  return fgetc(stdin);
 }
 
 int putchar(int c) {
--- libyaclib/stdio.h
+++ libyaclib/stdio.h
@@ -4,14 +4,39 @@
 #include "stddef.h"
 
 #define EOF -1
+typedef size_t fpos_t;
+
+typedef struct {
+  unsigned int fd;
+  unsigned int eof:1;
+  unsigned int err:12;
+  fpos_t pos;
+} FILE;
+
+extern FILE *stdin;
+extern FILE *stdout;
+extern FILE *stderr;
+
+// Error handling.
+int feof(FILE *stream);
+int ferror(FILE *stream);
+void clearerr(FILE *stream);
+
+// Direct IO.
+size_t fread(void * restrict buffer, size_t size, size_t count, FILE * restrict stream);
+size_t fwrite(void * restrict buffer, size_t size, size_t count, FILE * restrict stream);
+
+int fgetc(FILE *stream);
+#define getc(s) fgetc(s)
+
+int fgetc(FILE *stream);
 
 int getchar();
 int putchar(int c);
 
 int puts(const char *s);
 
-// TODO: unlocked_stdio
-
+// Formatted IO.
 int vprintf(const char * restrict format, va_list args);
 int vsprintf(char * restrict buffer, const char * restrict format, va_list args);
 int vsnprintf(char * restrict buffer, size_t buf_size, const char * restrict format, va_list args);
