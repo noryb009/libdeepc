f3e61ea46d94b03478fcbc53ac19139bfd65c5c8
Lukas Lorimer <lukas.lorimer@gmail.com>
2018-05-17 21:52:27 -0400

    Add offsetof

--- include/stddef.h
+++ include/stddef.h
@@ -6,3 +6,5 @@ typedef __size_t size_t;
 typedef __intptr_t ptrdiff_t;
 #define NULL __NULL
 typedef unsigned long long max_align_t;
+
+#define offsetof(type, member) __builtin_offsetof(type, member)
